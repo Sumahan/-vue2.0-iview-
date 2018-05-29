d493cad971fc02c5b7c1141cf12ed778a557ab42
hanpanpan <287781020@qq.com>
2018-05-29 19:31:38 +0800

    路由的匹配以及嵌套

---
+++ src/components/user.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user">
+    <div @click="goback()">Usersfsfd</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    goback () {
+      console.log(this.$route)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login'
+import User from '@/components/user'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -10,6 +12,17 @@ export default new Router({
       path: '/',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/user/:id',
+      name: 'user',
+      component: User,
+      children: [
+        {
+          path: 'HelloWorld',
+          component: HelloWorld
+        }
+      ]
     }
   ]
 })
